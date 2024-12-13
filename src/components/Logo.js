b6fe53fc00706920d4aabda4886b0c7e9843dd96
Aman Verma <amanverma740895@gmail.com>
2024-12-13 22:25:22 +0530

    diploy fronted

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logoname from "../assest/janshoplogo/logoname.png"
+const Logo = () => {
+  return (
+    
+    <div>
+    <img src={logoname} alt="" className='w-36 px-2'/>
+    </div>
+    
+  )
+}
+
+export default Logo
